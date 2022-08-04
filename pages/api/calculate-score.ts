3ac5246b6034c6f13dc6ffc6b38a57fba3649f06
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2022-08-04 15:41:51 +0200

    api: add placeholder endpoint

---
+++ pages/api/calculate-score.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { RequestData, ResponseData } from "../../types";
+
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{
+    error?: string,
+    data?: ResponseData,
+  }>
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ 'error': 'invalid method'});
+  }
+  const data: RequestData = JSON.parse(req.body);
+  console.log('Received', data)
+  res.status(200).json({ data: { score: 0 } })
+}
---
+++ types.ts
@@ -0,0 +1,2 @@
+export type RequestData = { pins: number }
+export type ResponseData = { score: number }
